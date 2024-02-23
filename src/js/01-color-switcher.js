c2206bc5862986b8fe086ac57f22c6e1c6d6ff93
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-23 23:49:27 +0100

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const start = document.querySelector('button[data-start]');
+const stopChange = document.querySelector('button[data-stop]');
+let startInterval = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+start.addEventListener('click', clickStart);
+
+function clickStart() {
+  startInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  start.disabled = true;
+  stopChange.disabled = false;
+}
+
+stopChange.addEventListener('click', clickStop);
+
+function clickStop() {
+  clearInterval(startInterval);
+  start.disabled = false;
+  stopChange.disabled = true;
+}
