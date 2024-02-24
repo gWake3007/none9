e01fff42d5606b275736a0dd68b1586c222b0d6d
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-24 17:26:00 +0000

    Deploying to gh-pages from @ gWake3007/none9@4b2643315e7db7258ef502c0c9d164a97d773fc2 🚀

---
+++ 03-promises.6e4e09cd.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".form"),button:document.querySelector("button")};e.button.addEventListener("click",(function(o){o.preventDefault();const t=e.form.elements.delay.value,n=e.form.elements.step.value,l=e.form.elements.amount.value;console.log(t),console.log(n),console.log(l)}));
+//# sourceMappingURL=03-promises.6e4e09cd.js.map
