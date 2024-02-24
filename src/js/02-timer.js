4b2643315e7db7258ef502c0c9d164a97d773fc2
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-24 18:25:39 +0100

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,38 @@
+const flatpickr = require('flatpickr');
+
+import flatpickr from 'flatpickr';
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+  input: document.getElementById('datetime-picker'),
+  button: document.querySelector('button[data-start]'),
+  dataDays: document.querySelector('span[data-days]'),
+  dataHours: document.querySelector('span[data-hours]'),
+  dataMinutes: document.querySelector('span[data-minutes]'),
+  dataSeconds: document.querySelector('span[data-seconds]'),
+};
+
+const currentDate = new Date();
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates.length > 0) {
+      const selectedDate = selectedDates[0];
+      const days = selectedDate.getDay();
+      const hours = selectedDate.getHours();
+      const minutes = selectedDate.getMinutes();
+      const seconds = selectedDate.getSeconds();
+    }
+  },
+};
+
+flatpickr(refs.input, options);
+
+refs.button.addEventListener('click', clickEvent);
+
+function clickEvent(event) {}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+const refs = {
+  form: document.querySelector('.form'),
+  button: document.querySelector('button'),
+};
+
+refs.button.addEventListener("click", clickSubmit);
+
+function clickSubmit(event) {
+  event.preventDefault();
+  const inputDelay = refs.form.elements.delay.value;
+  const inputStep = refs.form.elements.step.value;
+  const inputAmount = refs.form.elements.amount.value;
+  console.log(inputDelay);
+  console.log(inputStep);
+  console.log(inputAmount);
 }
