955fa35ebe88fec5774cd512e4ce89e0250b4435
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-25 19:27:31 +0000

    Deploying to gh-pages from @ gWake3007/none9@27b0963db80966851f6eecc409b54adf8e2099cd 🚀

--- 03-promises.037a627c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".form"),button:document.querySelector("button")};e.button.addEventListener("click",(function(o){o.preventDefault();var t=e.form.elements.delay.value,n=e.form.elements.step.value,l=e.form.elements.amount.value;console.log(t),console.log(n),console.log(l)}))}();
-//# sourceMappingURL=03-promises.037a627c.js.map
