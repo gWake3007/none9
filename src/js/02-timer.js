5cbc97752932ad7473e5d85c18903b7657024633
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-26 09:04:48 +0100

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
 import flatpickr from 'flatpickr';
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
 const flatpickr = require('flatpickr');
 
@@ -12,7 +12,11 @@ const refs = {
   dataSeconds: document.querySelector('span[data-seconds]'),
 };
 
-const currentDate = new Date();
+refs.button.disabled = true;
+
+let timerInterval = null; //?Потрібен для скасування інтервалу коли закінчеться час.Щоб відлік дойшов тільки до 0.
+let targetDate;
+let currentDate = new Date();
 
 const options = {
   enableTime: true,
@@ -20,22 +24,61 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (selectedDates.length > 0) {
-      const selectedDate = selectedDates[0];
-      const days = selectedDate.getDay();
-      const hours = selectedDate.getHours();
-      const minutes = selectedDate.getMinutes();
-      const seconds = selectedDate.getSeconds();
+    if (selectedDates[0] < currentDate) {
+      alert('Please choose a date in the future');
+    } else {
+      refs.button.disabled = false;
+      targetDate = selectedDates[0];
     }
   },
 };
 
-flatpickr(refs.input, options);
-
 refs.button.addEventListener('click', clickEvent);
 
-function clickEvent(event) {
-  options.onClose();
+flatpickr(refs.input, options);
+
+function clickEvent() {
+  timerInterval = setInterval(() => {
+    let date = new Date(); //?Тут створюємо нову дату let щоб в інтервалі шов відлік кожну секунду.
+    const { days, hours, minutes, seconds } = convertMs(
+      //?Деструкторизацією дістаємо всі значення.
+      targetDate.getTime() - date.getTime()
+    );
+
+    if (targetDate.getTime() - date.getTime() < 920) {
+      //?Перевірка чи не залишилося до кінця таймеру меньше 1000 мілісекунд.
+      clearInterval(timerInterval); //?Скасування інтервалу.
+      refs.button.disabled = true;
+    }
+
+    refs.dataDays.textContent = correctDate(days);
+    refs.dataHours.textContent = correctDate(hours);
+    refs.dataMinutes.textContent = correctDate(minutes);
+    refs.dataSeconds.textContent = correctDate(seconds);
+  }, 1000);
 }
 
+function convertMs(ms) {
+  //?Функція для переведення з getTime() - в дату.
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function correctDate(value) {
+  //?Функція для конвертації дати і часу до формату двух чисел(приклад: 02).
+  return value.toString().padStart(2, 0);
+}
